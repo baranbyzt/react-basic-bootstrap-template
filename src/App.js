27b85a7f5269e15b64ed522c60ce06b6259ec415
baran beyazıt <byztmain2121@gmail.com>
2022-02-04 19:40:27 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -4,9 +4,42 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="app container">
-  sadfdsafsdf
+    <>
+    
+    <div className='wrapper container '>
+<div className='row'>
+  
+<div className='section_1 col-12 col-lg-6'> 
+  <h2>Mus Sodales Nulla Integer</h2>
+  <p>Auctor Diam Sed Non Sapien In Risuultrice s nunc vitae posuere orci dui s Rhoncus Egestas Mauris Rhoncus Mi At Odio In Luctus Turpis Quis Dignissim.</p>
+  </div>
+<div className='section_2 col-12 col-lg-6'>
+
+<div className='row'>
+<div className='inner_sections sec_1 col-12 col-lg-6 col-12 '> 
+<h3>Interdum</h3>
+<p>Suspendisse elit praesent hendrerit lacus a condimentum scelerisque augue mi ultrices nunc vitae posuere orci dui quisque.</p>
+</div>
+<div className='inner_sections col-12 col-lg-6'> 
+<h3>Interdum</h3>
+<p>Suspendisse elit praesent hendrerit lacus a condimentum scelerisque augue mi ultrices nunc vitae posuere orci dui quisque.</p>
+</div>
+<div className='inner_sections col-12 col-lg-6'> 
+<h3>Interdum</h3>
+<p>Suspendisse elit praesent hendrerit lacus a condimentum scelerisque augue mi ultrices nunc vitae posuere orci dui quisque.</p>
+</div>
+<div className='inner_sections col-12 col-lg-6'> 
+<h3>Interdum</h3>
+<p>Suspendisse elit praesent hendrerit lacus a condimentum scelerisque augue mi ultrices nunc vitae posuere orci dui quisque.</p>
+</div>
+</div>
+
+</div>
+
+</div>
     </div>
+    
+    </>
   );
 }
 
